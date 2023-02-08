36557720284a29a78a3e02ba505ee961f897d51c
Harsh_324 <harshravi324@gmail.com>
2023-02-09 00:34:16 +0530

    mongodb connection done

--- scrap_Eleclerc/scrap_Eleclerc/pipelines.py
+++ scrap_Eleclerc/scrap_Eleclerc/pipelines.py
@@ -6,8 +6,19 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import pymongo
 
 class ScrapEleclercPipeline:
+
+    def __init__(self) -> None:
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+
+        db = self.conn['scrap']
+        self.collection = db['scrap_db1']
+
     def process_item(self, item, spider):
+        self.collection.insert_one(dict(item))
         return item
